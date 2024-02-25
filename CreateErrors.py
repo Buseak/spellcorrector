8de3b2b64598ecbdb19302bc3cbfaf165f91724d
buseak <buseak_98@hotmail.com>
2024-02-25 10:33:54 +0300

    Boun_test_v4

--- CreateErrors.py
+++ CreateErrors.py
@@ -191,7 +191,7 @@ def parse_sents(path):
 
   return(sentence_text_list)
 
-train_path = "tr_boun-ud-train.conllu"
+train_path = "tr_boun-ud-test.conllu"
 sentence_list = parse_sents(train_path)
 
 
@@ -224,7 +224,7 @@ def apply_errors(error_count_by_word, error_percent_by_sent,sentence_list, error
     input_sents = []
     output_sents = []
 
-    for sentence in sentence_list[:7000]:
+    for sentence in sentence_list:
         sent_errors = [0,0,0,0,0,0,0,0]
         
         words = sentence.split()
@@ -245,33 +245,49 @@ def apply_errors(error_count_by_word, error_percent_by_sent,sentence_list, error
                 min_error_index = error_counts.index(min(error_counts))
                 rand_error_position = random.randint(1,len(words[word_ind])-2)
                 word = words[word_ind]
-                error_by_word[word_ind] += 1
+                if min_error_index in [2,3,4]:
+                    #wordün keyboard dictte charı varsa onu seç
+                    if any(x in keyboard_closest_chars.keys() for x in list(word)):
+                        for q in range(len(word)):
+                            if word[q] in keyboard_closest_chars.keys():
+                                rand_error_position = q
+                    #yoksa başka hata seç
+                    else:
+                        min_error_index = random.choice([0,1,5,6,7])
                 match min_error_index:
                     case 0: #change_chars_prev
                         errored_word, input_word, output_word = change_chars_prev(word,rand_error_position)
                         error_counts[0] += 1
                         sent_errors[0] += 1
                         error_types[word_ind] = 1
+                        error_by_word[word_ind] += 1
                     case 1: #change_chars_prev
                         errored_word, input_word, output_word = change_chars_next(word,rand_error_position)
                         error_counts[1] += 1
                         sent_errors[1] += 1
                         error_types[word_ind] = 2
+                        error_by_word[word_ind] += 1
                     case 2: #change_chars_prev
-                        errored_word, input_word, output_word = add_random_char_prev(word,rand_error_position)
-                        error_counts[2] += 1
-                        sent_errors[2] += 1
-                        error_types[word_ind] = 3
+                        if word[rand_error_position] in keyboard_closest_chars.keys():
+                            errored_word, input_word, output_word = add_random_char_prev(word,rand_error_position)
+                            error_counts[2] += 1
+                            sent_errors[2] += 1
+                            error_types[word_ind] = 3
+                            error_by_word[word_ind] += 1
                     case 3: #change_chars_prev
-                        errored_word, input_word, output_word = add_random_char_next(word,rand_error_position)
-                        error_counts[3] += 1
-                        sent_errors[3] += 1
-                        error_types[word_ind] = 4
+                        if word[rand_error_position] in keyboard_closest_chars.keys():
+                            errored_word, input_word, output_word = add_random_char_next(word,rand_error_position)
+                            error_counts[3] += 1
+                            sent_errors[3] += 1
+                            error_types[word_ind] = 4
+                            error_by_word[word_ind] += 1
                     case 4: #change_chars_prev
-                        errored_word, input_word, output_word = change_with_random_char(word,rand_error_position)
-                        error_counts[4] += 1
-                        sent_errors[4] += 1
-                        error_types[word_ind] = 5
+                        if word[rand_error_position] in keyboard_closest_chars.keys():
+                            errored_word, input_word, output_word = change_with_random_char(word,rand_error_position)
+                            error_counts[4] += 1
+                            sent_errors[4] += 1
+                            error_types[word_ind] = 5
+                            error_by_word[word_ind] += 1
                     case 5: #change_chars_prev
                         indexes = [i for i, x in enumerate(list(word)) if x in consonants]
                         if len(indexes) > 1:
@@ -282,6 +298,7 @@ def apply_errors(error_count_by_word, error_percent_by_sent,sentence_list, error
                             error_counts[5] += 1
                             sent_errors[5] += 1
                             error_types[word_ind] = 6
+                            error_by_word[word_ind] += 1
                         else:
                             errored_word, input_word, output_word = word, add_dummy_char(word), add_dummy_char(word)
                     case 6: #change_chars_prev
@@ -294,6 +311,7 @@ def apply_errors(error_count_by_word, error_percent_by_sent,sentence_list, error
                             error_counts[6] += 1
                             sent_errors[6] += 1
                             error_types[word_ind] = 7
+                            error_by_word[word_ind] += 1
                         else:
                             errored_word, input_word, output_word = word, add_dummy_char(word), add_dummy_char(word)
                     case 7: #change_chars_prev
@@ -301,6 +319,7 @@ def apply_errors(error_count_by_word, error_percent_by_sent,sentence_list, error
                         error_counts[7] += 1
                         sent_errors[7] += 1
                         error_types[word_ind] = 8
+                        error_by_word[word_ind] += 1
 
                 words[word_ind] = errored_word
                 input_words[word_ind] = input_word
@@ -322,104 +341,104 @@ def apply_errors(error_count_by_word, error_percent_by_sent,sentence_list, error
                 output_words[w] = out_word
         input_sents.append(" ".join(input_words))
         output_sents.append(" ".join(output_words))
-    for sentence in sentence_list[7000:]:
-        sent_errors = [0,0,0,0,0,0,0,0]
+    # for sentence in sentence_list[7000:]:
+    #     sent_errors = [0,0,0,0,0,0,0,0]
         
-        words = sentence.split()
-        error_by_word = [0] * len(words)
-        error_types = [0] * len(words)
-        input_words = [""] * len(words)
-        output_words = [""] * len(words)
-        sent_len = math.ceil(len(words)/(1/error_percent_by_sent))
+    #     words = sentence.split()
+    #     error_by_word = [0] * len(words)
+    #     error_types = [0] * len(words)
+    #     input_words = [""] * len(words)
+    #     output_words = [""] * len(words)
+    #     sent_len = math.ceil(len(words)/(1/error_percent_by_sent))
         
-        available_words = get_index_of_available_words(sentence)
+    #     available_words = get_index_of_available_words(sentence)
 
-        new_sent = []
+    #     new_sent = []
 
-        if len(available_words) >= sent_len:
-            rand_word_indices = random.sample(available_words,sent_len)
-            rand_word_indices.sort()
-            for word_ind in rand_word_indices:
-                min_error_index = random.choice([5,6,7])
-                rand_error_position = random.randint(1,len(words[word_ind])-2)
-                word = words[word_ind]
-                error_by_word[word_ind] += 1
-                match min_error_index:
-                    case 0: #change_chars_prev
-                        errored_word, input_word, output_word = change_chars_prev(word,rand_error_position)
-                        error_counts[0] += 1
-                        sent_errors[0] += 1
-                        error_types[word_ind] = 1
-                    case 1: #change_chars_prev
-                        errored_word, input_word, output_word = change_chars_next(word,rand_error_position)
-                        error_counts[1] += 1
-                        sent_errors[1] += 1
-                        error_types[word_ind] = 2
-                    case 2: #change_chars_prev
-                        errored_word, input_word, output_word = add_random_char_prev(word,rand_error_position)
-                        error_counts[2] += 1
-                        sent_errors[2] += 1
-                        error_types[word_ind] = 3
-                    case 3: #change_chars_prev
-                        errored_word, input_word, output_word = add_random_char_next(word,rand_error_position)
-                        error_counts[3] += 1
-                        sent_errors[3] += 1
-                        error_types[word_ind] = 4
-                    case 4: #change_chars_prev
-                        errored_word, input_word, output_word = change_with_random_char(word,rand_error_position)
-                        error_counts[4] += 1
-                        sent_errors[4] += 1
-                        error_types[word_ind] = 5
-                    case 5: #change_chars_prev
-                        indexes = [i for i, x in enumerate(list(word)) if x in consonants]
-                        if len(indexes) > 1:
-                            rand_consonant_position = random.randint(1,len(indexes)-1)
-                            if indexes[rand_consonant_position] == len(word)-1:
-                                rand_consonant_position += -1
-                            errored_word, input_word, output_word = del_char(word,indexes[rand_consonant_position])
-                            error_counts[5] += 1
-                            sent_errors[5] += 1
-                            error_types[word_ind] = 6
-                        else:
-                            errored_word, input_word, output_word = word, add_dummy_char(word), add_dummy_char(word)
-                    case 6: #change_chars_prev
-                        indexes = [i for i, x in enumerate(list(word)) if x in vowel]
-                        if len(indexes) > 1:
-                            rand_vowel_position = random.randint(1,len(indexes)-1)
-                            if indexes[rand_vowel_position] == len(word)-1:
-                                rand_vowel_position += -1
-                            errored_word, input_word, output_word = del_char(word,indexes[rand_vowel_position])
-                            error_counts[6] += 1
-                            sent_errors[6] += 1
-                            error_types[word_ind] = 7
-                        else:
-                            errored_word, input_word, output_word = word, add_dummy_char(word), add_dummy_char(word)
-                    case 7: #change_chars_prev
-                        errored_word, input_word, output_word = same_char_repetition(word,rand_error_position)
-                        error_counts[7] += 1
-                        sent_errors[7] += 1
-                        error_types[word_ind] = 8
+    #     if len(available_words) >= sent_len:
+    #         rand_word_indices = random.sample(available_words,sent_len)
+    #         rand_word_indices.sort()
+    #         for word_ind in rand_word_indices:
+    #             min_error_index = random.choice([5,6,7])
+    #             rand_error_position = random.randint(1,len(words[word_ind])-2)
+    #             word = words[word_ind]
+    #             error_by_word[word_ind] += 1
+    #             match min_error_index:
+    #                 case 0: #change_chars_prev
+    #                     errored_word, input_word, output_word = change_chars_prev(word,rand_error_position)
+    #                     error_counts[0] += 1
+    #                     sent_errors[0] += 1
+    #                     error_types[word_ind] = 1
+    #                 case 1: #change_chars_prev
+    #                     errored_word, input_word, output_word = change_chars_next(word,rand_error_position)
+    #                     error_counts[1] += 1
+    #                     sent_errors[1] += 1
+    #                     error_types[word_ind] = 2
+    #                 case 2: #change_chars_prev
+    #                     errored_word, input_word, output_word = add_random_char_prev(word,rand_error_position)
+    #                     error_counts[2] += 1
+    #                     sent_errors[2] += 1
+    #                     error_types[word_ind] = 3
+    #                 case 3: #change_chars_prev
+    #                     errored_word, input_word, output_word = add_random_char_next(word,rand_error_position)
+    #                     error_counts[3] += 1
+    #                     sent_errors[3] += 1
+    #                     error_types[word_ind] = 4
+    #                 case 4: #change_chars_prev
+    #                     errored_word, input_word, output_word = change_with_random_char(word,rand_error_position)
+    #                     error_counts[4] += 1
+    #                     sent_errors[4] += 1
+    #                     error_types[word_ind] = 5
+    #                 case 5: #change_chars_prev
+    #                     indexes = [i for i, x in enumerate(list(word)) if x in consonants]
+    #                     if len(indexes) > 1:
+    #                         rand_consonant_position = random.randint(1,len(indexes)-1)
+    #                         if indexes[rand_consonant_position] == len(word)-1:
+    #                             rand_consonant_position += -1
+    #                         errored_word, input_word, output_word = del_char(word,indexes[rand_consonant_position])
+    #                         error_counts[5] += 1
+    #                         sent_errors[5] += 1
+    #                         error_types[word_ind] = 6
+    #                     else:
+    #                         errored_word, input_word, output_word = word, add_dummy_char(word), add_dummy_char(word)
+    #                 case 6: #change_chars_prev
+    #                     indexes = [i for i, x in enumerate(list(word)) if x in vowel]
+    #                     if len(indexes) > 1:
+    #                         rand_vowel_position = random.randint(1,len(indexes)-1)
+    #                         if indexes[rand_vowel_position] == len(word)-1:
+    #                             rand_vowel_position += -1
+    #                         errored_word, input_word, output_word = del_char(word,indexes[rand_vowel_position])
+    #                         error_counts[6] += 1
+    #                         sent_errors[6] += 1
+    #                         error_types[word_ind] = 7
+    #                     else:
+    #                         errored_word, input_word, output_word = word, add_dummy_char(word), add_dummy_char(word)
+    #                 case 7: #change_chars_prev
+    #                     errored_word, input_word, output_word = same_char_repetition(word,rand_error_position)
+    #                     error_counts[7] += 1
+    #                     sent_errors[7] += 1
+    #                     error_types[word_ind] = 8
 
-                words[word_ind] = errored_word
-                input_words[word_ind] = input_word
-                output_words[word_ind] = output_word
-            new_sent.append(words)
-            modified_sent_list.append(" ".join(new_sent[0]))
-        else:
-            modified_sent_list.append(sentence)
-        error_by_sent.append(sent_errors)
-        error_counts_by_word.append(error_by_word)
-        error_type_by_word.append(error_types)
+    #             words[word_ind] = errored_word
+    #             input_words[word_ind] = input_word
+    #             output_words[word_ind] = output_word
+    #         new_sent.append(words)
+    #         modified_sent_list.append(" ".join(new_sent[0]))
+    #     else:
+    #         modified_sent_list.append(sentence)
+    #     error_by_sent.append(sent_errors)
+    #     error_counts_by_word.append(error_by_word)
+    #     error_type_by_word.append(error_types)
 
-        for w in range(len(input_words)):
-            if input_words[w] == "":
-                inp_word = add_dummy_char(words[w])
-                input_words[w] = inp_word
-            if output_words[w] == "":
-                out_word = add_dummy_char(words[w])
-                output_words[w] = out_word
-        input_sents.append(" ".join(input_words))
-        output_sents.append(" ".join(output_words))
+    #     for w in range(len(input_words)):
+    #         if input_words[w] == "":
+    #             inp_word = add_dummy_char(words[w])
+    #             input_words[w] = inp_word
+    #         if output_words[w] == "":
+    #             out_word = add_dummy_char(words[w])
+    #             output_words[w] = out_word
+    #     input_sents.append(" ".join(input_words))
+    #     output_sents.append(" ".join(output_words))
 
     return modified_sent_list, error_counts, error_by_sent, error_counts_by_word, input_sents, output_sents, error_type_by_word
 
@@ -427,10 +446,10 @@ modified_sentences, errors, errors_for_sentence, errors_for_word, input_sentence
 
 
 df = pd.DataFrame({"Original_sent": sentence_list, "Error_sent": modified_sentences, "Errors_for_sentence": errors_for_sentence, "Errors_for_word": errors_for_word, "Error_type_by_word": error_type_by_word})
-df.to_excel("boun_train_spelling_mistakes_050_qwerty_v4.xlsx")
+df.to_excel("boun_test_spelling_mistakes_050_qwerty_v4.xlsx")
 
 df_inp_out = pd.DataFrame({"Input_sentences": input_sentences, "Output_sentences": output_sentences})
-df_inp_out.to_excel("boun_train_050_inp_out_qwerty_v4.xlsx")
+df_inp_out.to_excel("boun_test_050_inp_out_qwerty_v4.xlsx")
 
 
 print(errors)
